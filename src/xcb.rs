c551a3eb2efc26b599bc2a023ebe9372a7675642
André Wagner <andre.nho@gmail.com>
2015-07-19 21:49:22 -0300

    .

--- src/xcb.rs
+++ src/xcb.rs
@@ -53,6 +53,7 @@ impl Connection {
 
     fn create_simple_window(&self, w: u16, h: u16, border: u16, values: Vec<(u32, u32)>) -> Window {
         let w = Window { id: unsafe { xcb_generate_id(self.conn) } };
+        xcb_create_window(self.conn, 0 /* copy from parent */, w.id, 
     //fn xcb_create_window(conn: *mut c_void, depth: u8, wid: xcb_window_t, parent: xcb_window_t,
     //                     x: i16, y: i16, w: u16, h: u16, border: u16, _class: u16, visual: xcb_visualid_t,
     //                     value_mask: u32, value_list: *const u32) -> xcb_void_cookie_t;
