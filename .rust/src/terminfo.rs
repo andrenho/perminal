f7406f3f4f4c95c54e0cc668593b114226cdcb4f
André Wagner <andre.nho@gmail.com>
2015-08-28 23:23:23 -0300

    Cleaning state for a new version.

--- .rust/src/terminfo.rs
+++
@@ -1,9 +0,0 @@
-use command::Command;
-use userevent::Key;
-
-pub trait Terminfo {
-    fn parse_output(&mut self, c: u8) -> Vec<Command>;
-    fn parse_input(&self, key: &Key) -> Vec<u8>;
-}
-
-// vim: ts=4:sw=4:sts=4:expandtab
